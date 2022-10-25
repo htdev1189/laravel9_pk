92f5fbae775c5b8f0e55e686593b02e2c4b47b4d
htdev <htdev1189@gmail.com>
2022-10-25 17:09:41 +0700

    laravel pusher

--- laravel.sql
+++ laravel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 21, 2022 at 04:45 AM
+-- Generation Time: Oct 25, 2022 at 10:09 AM
 -- Server version: 5.7.36
 -- PHP Version: 8.1.0
 
@@ -24,11 +24,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categories`
+-- Table structure for table `admins`
 --
 
-DROP TABLE IF EXISTS `categories`;
-CREATE TABLE IF NOT EXISTS `categories` (
+DROP TABLE IF EXISTS `admins`;
+CREATE TABLE IF NOT EXISTS `admins` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `group` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `admin_username_unique` (`username`),
+  UNIQUE KEY `admin_email_unique` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `username`, `password`, `name`, `email`, `group`, `created_at`, `updated_at`, `image`) VALUES
+(2, 'admin', '$2y$10$DnR7HhUlmWjAPNTUCL2rJ.RWK7MytlrR.Vcl3NEuMxh6vUXZwvT.O', 'admin', 'admin@locallhost.com', 1, '2022-10-21 08:16:19', '2022-10-25 05:06:42', 'http://laravel.htdev/upload/images/tacgia.jpeg'),
+(4, 'bientap', '$2y$10$KITHE2d3S/mQSHr8GDFxK.CLldf7CfjqIVxlHEHP8hAd.pS/kfwGu', 'bien tap', 'bientap@gmail.com', 2, '2022-10-22 01:19:40', '2022-10-25 05:06:57', 'http://laravel.htdev/upload/images/tacgia.jpeg'),
+(5, 'tuvan', '$2y$10$.qs8VCWN0hLFxBt3OC0rQenu9YJF/ZEYnLeVxMpjFQIoEq074.9Oa', 'tu van dien thoai', 'tuvan@gmail.com', 3, '2022-10-22 01:20:45', '2022-10-25 05:07:04', 'http://laravel.htdev/upload/images/tacgia.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categorys`
+--
+
+DROP TABLE IF EXISTS `categorys`;
+CREATE TABLE IF NOT EXISTS `categorys` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `slug` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -39,22 +70,7 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `updated_at` timestamp NULL DEFAULT NULL,
   `description` text COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `categories`
---
-
-INSERT INTO `categories` (`id`, `name`, `slug`, `parent`, `image`, `status`, `created_at`, `updated_at`, `description`) VALUES
-(8, 'nam khoa', 'nam-khoa', 0, 'http://laravel.htdev/upload/images/logo-category/namkhoa.svg', 1, '2022-10-18 02:45:15', '2022-10-21 04:43:01', '<p>Interactively procrastinate high-payoff content without backward-compatible data. Quickly cultivate optimal processes and tactical architectures.</p>'),
-(9, 'phụ khoa', 'phu-khoa', 0, 'http://laravel.htdev/upload/images/logo-category/phukhoa.svg', 1, '2022-10-18 02:45:58', '2022-10-21 04:43:24', '<p>Interactively procrastinate high-payoff content without backward-compatible data. Quickly cultivate optimal processes and tactical architectures.</p>'),
-(10, 'bệnh xã hội', 'benh-xa-hoi', 0, 'http://laravel.htdev/upload/images/logo-category/benhxahoi.svg', 1, '2022-10-18 02:46:40', '2022-10-21 04:43:33', '<p>Interactively procrastinate high-payoff content without backward-compatible data. Quickly cultivate optimal processes and tactical architectures.</p>'),
-(11, 'bệnh trĩ', 'benh-tri', 0, 'http://laravel.htdev/upload/images/logo-category/benhtri.svg', 1, '2022-10-18 02:47:07', '2022-10-21 04:43:45', '<p>Interactively procrastinate high-payoff content without backward-compatible data. Quickly cultivate optimal processes and tactical architectures.</p>'),
-(12, 'tai mũi họng', 'tai-mui-hong', 0, 'http://laravel.htdev/upload/images/logo-category/taimuihong.svg', 1, '2022-10-18 02:47:36', '2022-10-21 04:43:56', '<p>Interactively procrastinate high-payoff content without backward-compatible data. Quickly cultivate optimal processes and tactical architectures.</p>'),
-(13, 'đình chỉ thai', 'dinh-chi-thai', 0, 'http://laravel.htdev/upload/images/logo-category/dinhchithai.svg', 1, '2022-10-18 02:48:03', '2022-10-21 04:44:07', '<p>Interactively procrastinate high-payoff content without backward-compatible data. Quickly cultivate optimal processes and tactical architectures.</p>'),
-(14, 'bao quy đầu 1', 'bao-quy-dau-1', 8, NULL, 1, '2022-10-18 02:49:12', '2022-10-18 21:06:17', NULL),
-(15, 'viêm phụ khoa', 'viem-phu-khoa', 9, NULL, 1, '2022-10-18 19:26:16', '2022-10-18 19:26:16', NULL),
-(16, 'sùi mào gà', 'sui-mao-ga', 10, NULL, 1, '2022-10-18 20:03:10', '2022-10-18 20:03:10', NULL);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -87,21 +103,22 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `migrations`
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(5, '2022_10_19_041403_create_table_post', 1),
+(13, '2014_10_12_000000_create_users_table', 2),
+(14, '2014_10_12_100000_create_password_resets_table', 2),
+(15, '2019_08_19_000000_create_failed_jobs_table', 2),
+(16, '2019_12_14_000001_create_personal_access_tokens_table', 2),
 (6, '2022_10_21_101946_create_tongdai_table', 1),
-(7, '2022_10_21_103102_create_categories_table', 1),
-(8, '2022_10_21_114041_add_description_column_to_categories_table', 2);
+(17, '2022_10_19_041403_create_table_post', 2),
+(18, '2022_10_21_103102_create_categories_table', 2),
+(19, '2022_10_21_114041_add_description_column_to_categories_table', 2),
+(21, '2022_10_25_090421_create_thongbao_table', 3);
 
 -- --------------------------------------------------------
 
@@ -151,7 +168,7 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `slug` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `category` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
   `image` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `description` text COLLATE utf8mb4_unicode_ci,
   `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -159,17 +176,36 @@ CREATE TABLE IF NOT EXISTS `posts` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `posts`
+-- Table structure for table `thongbaos`
 --
 
-INSERT INTO `posts` (`id`, `title`, `slug`, `category`, `image`, `description`, `content`, `status`, `created_at`, `updated_at`) VALUES
-(8, 'Examination for kids 2', 'examination-for-kids-2', 10, 'http://laravel.htdev/upload/images/img-blog-post-02-1200x800.jpg', '<p>Interactively procrastinate high-payoff content without backward-compatible data. Quickly cultivate optimal processes and tactical architectures.</p>', '<p>&nbsp;</p><p>&nbsp;</p><p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p><p>&nbsp;</p><figure class=\"image\"><img src=\"http://laravel.htdev/upload/images/img-blog-post-02-1200x800.jpg\"></figure><p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>', 1, '2022-10-21 01:21:27', '2022-10-21 01:21:27'),
-(7, 'Examination for kids 1', 'examination-for-kids-1', 9, 'http://laravel.htdev/upload/images/img-blog-post-01-1200x800.jpg', '<p>Interactively procrastinate high-payoff content without backward-compatible data. Quickly cultivate optimal processes and tactical architectures.</p>', '<p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p><figure class=\"image\"><img src=\"http://laravel.htdev/upload/images/img-blog-post-01-1200x800.jpg\"></figure><p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>', 1, '2022-10-21 01:19:37', '2022-10-21 01:19:37'),
-(6, 'Examination for kids', 'examination-for-kids', 8, 'http://laravel.htdev/upload/images/namkhoa/img-blog-post-01-1200x800.jpg', '<p>Interactively procrastinate high-payoff content without backward-compatible data. Quickly cultivate optimal processes and tactical architectures.</p>', '<p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p><figure class=\"image\"><img src=\"http://laravel.htdev/upload/images/namkhoa/img-blog-post-01-1200x800.jpg\"></figure><p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>', 1, '2022-10-21 01:17:56', '2022-10-21 01:17:56'),
-(9, 'Examination for kids 3', 'examination-for-kids-3', 8, 'http://laravel.htdev/upload/images/img-blog-post-02-1200x800.jpg', '<p>Interactively procrastinate high-payoff content without backward-compatible data. Quickly cultivate optimal processes and tactical architectures.</p>', '<p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p><p>&nbsp;</p><figure class=\"image\"><img src=\"http://laravel.htdev/upload/images/img-blog-post-02-1200x800.jpg\"></figure><p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>', 1, '2022-10-21 04:11:59', '2022-10-21 04:11:59');
+DROP TABLE IF EXISTS `thongbaos`;
+CREATE TABLE IF NOT EXISTS `thongbaos` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `to` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `seen` text COLLATE utf8mb4_unicode_ci,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `thongbaos`
+--
+
+INSERT INTO `thongbaos` (`id`, `title`, `admin_id`, `to`, `content`, `seen`, `status`, `created_at`, `updated_at`) VALUES
+(17, 'a', 2, '{\"4\":0,\"5\":0}', '<p>aaa</p>', NULL, 1, '2022-10-25 09:54:53', '2022-10-25 09:54:53'),
+(16, 'a', 2, '{\"4\":0,\"5\":0}', '<p>a</p>', NULL, 1, '2022-10-25 09:54:29', '2022-10-25 09:54:29'),
+(15, 'thong bao', 2, '{\"4\":0,\"5\":0}', '<p>thong bao</p>', NULL, 1, '2022-10-25 09:50:44', '2022-10-25 09:50:44');
 
 -- --------------------------------------------------------
 
@@ -187,7 +223,7 @@ CREATE TABLE IF NOT EXISTS `tongdai` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `tongdai`
@@ -200,7 +236,8 @@ INSERT INTO `tongdai` (`id`, `number`, `ip`, `note`, `status`, `created_at`, `up
 (4, '111', '::1', NULL, 0, '2022-10-21 04:07:32', '2022-10-21 04:07:32'),
 (5, '123', '::1', NULL, 0, '2022-10-21 04:09:39', '2022-10-21 04:09:39'),
 (6, '11111', '::1', NULL, 0, '2022-10-21 04:10:12', '2022-10-21 04:10:12'),
-(7, '25', '::1', NULL, 0, '2022-10-21 04:32:15', '2022-10-21 04:32:15');
+(7, '25', '::1', NULL, 0, '2022-10-21 04:32:15', '2022-10-21 04:32:15'),
+(8, '343434', '::1', NULL, 0, '2022-10-21 06:23:07', '2022-10-21 06:23:07');
 
 -- --------------------------------------------------------
 
